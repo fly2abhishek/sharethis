36b2bccfb6d570bcf9f1e651cd69d3f88a86adb9
naveenvalecha <er.naveenvalecha@gmail.com>
2015-12-28 13:32:06 +0530

    Issue : #2609720 Added a base test and define configuration schema for sharethis.

---
+++ src/Tests/SharethisConfigFormTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ */
+
+namespace Drupal\sharethis\Tests;
+use Drupal\simpletest\WebTestBase;
+
+
+/**
+ * Tests the SystemConfigFormTestBase class.
+ *
+ * @group Sharethis
+ */
+class SharethisConfigFormTest extends WebTestBase {
+
+
+  protected $adminUser;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('node', 'system_test', 'user', 'sharethis');
+
+  public function setUp() {
+    parent::setUp();
+    // Create and log in admin user.
+    $this->adminUser = $this->drupalCreateUser(['administer sharethis']);
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests the SharethisConfigForm.
+   */
+  function testSharethisConfigForm() {
+  }
+
+}
