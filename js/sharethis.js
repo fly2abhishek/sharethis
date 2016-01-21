e0dddef9ce49ab112c27cb9dee61537410c017a6
neetu morwani <neetumorwani@gmail.com>
2016-01-21 15:58:42 +0530

    Issue : #2609720 by neetu morwani.sharethis links updated

---
+++ js/sharethis.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * This file contains most of the code for the configuration page.
+ */
+
+(function ($, drupalSettings) {
+    'use strict';
+    $(document).ready(function(){
+        var switchTo5x = true;var __st_loadLate = true;
+        if (stLight !== undefined) {
+            stLight.options(drupalSettings.sharethis);
+        }
+
+    });
+})(jQuery, drupalSettings);
--- src/Plugin/Block/SharethisBlock.php
+++ src/Plugin/Block/SharethisBlock.php
@@ -94,5 +94,4 @@ class SharethisBlock extends BlockBase implements ContainerFactoryPluginInterfac
       ];
     }
   }
-
 }
